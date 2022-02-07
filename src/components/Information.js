2f09599f088cc064370f5c6b77c880685837d04b
kaderabdul <kaderabdul7980@gmail.com>
2022-02-07 15:53:23 +0600

    finish to make dynamic tab

--- src/components/Information.js
+++ src/components/Information.js
@@ -1,22 +1,24 @@
 import React from 'react';
+import { useState } from 'react';
 import CustomBtn from './elements/CustomBtn';
 import Request from './tabs/Request';
 import Reservation from './tabs/Reservation';
 import SendMessage from './tabs/SendMessage';
 
 const Information = () => {
+    const [tab,setTab] = useState('sendmessage')
     return (
         <div className='w-full h-screen bg-info-bg bg-cover bg-center text-white uppercase p-8'>
             
             <div className='children:m-4'>
-                <CustomBtn children='Send Message'></CustomBtn>
-                <CustomBtn children='Request Info'></CustomBtn>
-                <CustomBtn children='Make Reservation'></CustomBtn>
+                <CustomBtn children='Send Message' onClickItem={() => setTab('sendmessage')}></CustomBtn>
+                <CustomBtn children='Request Info' onClickItem={() => setTab('requestInfo')}></CustomBtn>
+                <CustomBtn children='Make Reservation' onClickItem={() => setTab('makeReservation')}></CustomBtn>
             </div>
 
-            <SendMessage></SendMessage>
-            <Request></Request>
-            <Reservation></Reservation>
+            {tab === 'sendmessage' && <SendMessage></SendMessage>}
+            {tab === 'requestInfo' && <Request></Request>}
+            {tab === 'makeReservation' && <Reservation></Reservation>}
 
         </div>
     );
